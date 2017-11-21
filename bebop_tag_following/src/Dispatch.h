ec110c4ea6856752e81abcfecba584238c81817a
Brennan Cain <Brennan@BrennanCain.com>
2017-11-21 16:31:14 -0500

    initial

---
+++ bebop_tag_following/src/Dispatch.h
@@ -0,0 +1,9 @@
+#ifndef BSC_DISPATCH_H
+#define BSC_DISPATCH_H
+
+class Dispatch{
+	public:
+		int entschuldigung;
+	private:
+		int bitte;
+};
---
+++ rotors_sim_tools/launch/runorb.sh
@@ -0,0 +1,2 @@
+rosrun ORB_SLAM2 Mono /home/brennan/ORB_SLAM2/Vocabulary/ORBvoc.txt /home/brennan/catkin_ws/src/birds-eye-view/bebop_tag_following/config/bebop_sim.yaml /camera/image_raw:=/bebop2/camera_base/image_raw
+
